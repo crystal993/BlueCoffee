b0227b705f588ef882d447923f3ec0b0be648446
DESKTOP-HCK0909\ggg71 <ggg7152@gmail.com>
2022-07-07 13:18:00 +0900

    커피 카테고리 router

---
+++ routes/coffee_route.py
@@ -0,0 +1,13 @@
+from flask import request, render_template, redirect, Blueprint, jsonify
+from pymongo import MongoClient
+import config
+
+client = MongoClient(config.DB_CLIENT)
+db = client.dbtoy
+
+bp = Blueprint('coffee', __name__, url_prefix='/coffee')
+
+# 조회(R) : 카페 리스트
+@bp.route('/list')
+def list():
+    return render_template('coffee/list.html')
