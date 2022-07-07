0416405f436c4457417f7126f4af2438265e76fb
DESKTOP-HCK0909\ggg71 <ggg7152@gmail.com>
2022-07-07 15:46:37 +0900

    제품 등록 페이지 이동 기능

--- routes/product_route.py
+++ routes/product_route.py
@@ -15,9 +15,22 @@ def list():
     token_receive = request.cookies.get('mytoken')
     if token_receive is not None:
         payload = jwt.decode(token_receive, config.JWT_SECRET_KEY, algorithms=['HS256'])
-        # user_info = db.users.find_one({"id": payload["id"]})
+        user_info = db.users.find_one({"user_id": payload["id"]})
         login_status = 1
-        return render_template('coffee/list.html', login_status=login_status)
+        return render_template('product/list.html', login_status=login_status, user_info=user_info)
     else:
         login_status = 0
-    return render_template('coffee/list.html', login_status=login_status)
+    return render_template('product/list.html', login_status=login_status)
+
+# 조회(R) : 상품 등록 페이지로 이동
+@bp.route('/form')
+def form():
+    token_receive = request.cookies.get('mytoken')
+    if token_receive is not None:
+        payload = jwt.decode(token_receive, config.JWT_SECRET_KEY, algorithms=['HS256'])
+        user_info = db.users.find_one({"user_id": payload["id"]})
+        login_status = 1
+        return render_template('product/form.html', login_status=login_status, user_info=user_info)
+    else:
+        login_status = 0
+    return render_template('product/form.html', login_status=login_status)
